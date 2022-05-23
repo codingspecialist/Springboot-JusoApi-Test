4a360fc761c7bf08142d1889674dfe7b0a6d9ba3
ssar <ssar@nate.com>
2022-05-23 14:08:47 +0900

    주소 찾기 - 방법1

--- src/main/java/site/metacoding/jusotest/JusoController.java
+++ src/main/java/site/metacoding/jusotest/JusoController.java
@@ -4,15 +4,36 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class JusoController {
 
     @GetMapping("/")
-    public String home(Model model) {
-        String jusoUrl = "https://www.juso.go.kr/addrlink/addrLinkUrl.do?confmKey=devU01TX0FVVEgyMDIyMDUyMzEwNDgzNTExMjYwMDA=&returnUrl=http://localhost:8080/juso/callback&resultType=4";
-        model.addAttribute("jusoUrl", jusoUrl);
+    public String home() {
         return "home";
+    }
+
+    @RequestMapping(value = "/juso/popup", method = { RequestMethod.GET, RequestMethod.POST })
+    public String jusoPopup(Model model, String roadFullAddr) {
+
+        String jusoUrl = "https://www.juso.go.kr/addrlink/addrLinkUrl.do";
+        String confmKey = "devU01TX0FVVEgyMDIyMDUyMzEwNDgzNTExMjYwMDA=";
+        String returnUrl = "http://localhost:8080/juso/popup";
+        String resultType = "4";
+
+        if (roadFullAddr == null) {
+            roadFullAddr = "N";
+        }
+
+        model.addAttribute("roadFullAddr", roadFullAddr);
+        model.addAttribute("jusoUrl", jusoUrl);
+        model.addAttribute("confmKey", confmKey);
+        model.addAttribute("returnUrl", returnUrl);
+        model.addAttribute("resultType", resultType);
+
+        return "jusoPopup";
     }
 
     @PostMapping("/juso/callback")
